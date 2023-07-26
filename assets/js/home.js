9a14a4e9238eb88f2198ec45bfc120575e431300
Giovannicampo <giovanni.campo144@gmail.com>
2023-07-26 02:46:16 +0200

    ulteriori modifiche homepage, quasi pronta

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,14 +10,39 @@ const main = () => {
                 count = 1;
                 return;
             }
-            section = document.querySelector(".section")
+            let section = document.querySelector(".section");
+            let home = document.getElementsByClassName("home")[0];
+            let description = document.querySelector(".home>.description");
+
+            home.className = "home";
+            switch ( count ) 
+            {
+                case 1:
+                    console.log("1");
+                    home.className += " img-bg-1";
+                    description.className = ("description white-text");
+                    break;
+                
+                case 2:
+                    console.log("2");
+                    home.className += (" img-bg-2");
+                    description.className = ("description transparent-text");
+                    break;
+                
+                case 3:
+                    console.log("3");
+                    home.className += (" img-bg-3 opposite-description");
+                    description.className = ("description white-text");
+                    break;
+            }
+
             section.innerHTML = `0${count++}/03`;
         }
     }
 
     setInterval(() => {
-        setTimeout(Home.changeBackground,3000)
+        setTimeout(Home.changeBackground,1000);
     }, 3000);
 }
 
-window.onload = main;
+window.onload = () => { main(); }
