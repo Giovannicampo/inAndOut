3fca5dee3402689a153d769243d9b8edd5e26684
Giovannicampo <giovanni.campo144@gmail.com>
2023-07-29 15:26:43 +0200

    si continua

--- assets/js/indoor.js
+++ assets/js/indoor.js
@@ -76,11 +76,6 @@ class Pin
             this.icon.className = "pin enlight";
             this.banner.setInvisible(true);
         })
-
-        window.onresize = () => {
-            this.icon.style.left = `${window.innerWidth - this.left_offset}px`;
-            this.banner.resetPos();
-        }
     }
 
     init ()
@@ -95,6 +90,14 @@ class Pin
         this.setEvents();
     }
 
+    resize ()
+    {
+        console.log(`${this.banner_name} resize`);
+        this.icon.style.left = `${window.innerWidth - this.left_offset}px`;
+        console.log(this.icon.style.left);
+        this.banner.resetPos();
+    }
+
     obj () 
     {
         return this.icon;
@@ -102,67 +105,57 @@ class Pin
 }
 
 const main = () => {
-    let pinIndoor;
-    let pinContemporaneo;
-    let pinJungle1;
-    let pinJungle2;
-    let pinEtnico1;
-    let pinEtnico2;
-    let pinIndustriale1;
-    let pinIndustriale2;
-    let pinMediterraneo;
-    let pinVintage1;
-    let pinVintage2;
-    let pinMinimalista;
-    let pinShabby;
-    let pinScandinavo;
+    // let pinIndoor;
+    // let pinContemporaneo;
+    // let pinJungle1;
+    // let pinJungle2;
+    // let pinEtnico1;
+    // let pinEtnico2;
+    // let pinIndustriale1;
+    // let pinIndustriale2;
+    // let pinMediterraneo;
+    // let pinVintage1;
+    // let pinVintage2;
+    // let pinMinimalista;
+    // let pinShabby;
+    // let pinScandinavo;
+
+    let pins = [];
 
     createPins = function () {
-        pinIndoor = new Pin(800,850, "LIDAS", "€149.90","Sedie e poltrone");
-        document.body.appendChild( pinIndoor.obj() );
+        pins.push( new Pin(800,850, "LIDAS", "€149.90","Sedie e poltrone") );
+        pins.push ( new Pin(2050,1250, "KIVIK", "€1599.90","Divani") );
+        pins.push( new Pin(3250,820, "FEJKA", "€34.99","Decorazioni casa") );
+        pins.push ( new Pin(2800,1550, "FLAKNAN", "€79.90","Decorazioni casa") );
+        pins.push( new Pin(4500,1000, "NAMMARO", "€249.90","Tavoli") );
+        pins.push ( new Pin(4000,1130, "BARLAST", "€80.90","Decorazioni casa") );
+        pins.push ( new Pin(5500,850, "LANDSKRONA", "€2490.90","Divani") );
+        pins.push ( new Pin(5700,1200, "TARSELE", "€100.90","Tavoli") );
+        pins.push ( new Pin(6600,1520, "AZUL", "€350.90","Sedie e poltrone") );
+        pins.push ( new Pin(7800,1500, "BILD", "€50.90","Decorazioni casa") );
+        pins.push ( new Pin(7940,900, "PINNTORP", "€189.90","Tavoli") );
+        pins.push ( new Pin(9300,1000, "MINIMAL", "€249.90","Sedie e poltrone") );
+        pins.push ( new Pin(10500,1100, "KASBAH", "€69.90","Sedie e poltrone") );
+        pins.push ( new Pin(11750,850, "THOR", "€120.90","Sedie e poltrone") );
 
-        pinContemporaneo = new Pin(2050,1250, "KIVIK", "€1599.90","Divani");
-        document.body.appendChild( pinContemporaneo.obj() );
-
-        pinJungle1 = new Pin(3250,820, "FEJKA", "€34.99","Decorazioni casa");
-        document.body.appendChild( pinJungle1.obj() );
-
-        pinJungle2 = new Pin(2800,1550, "FLAKNAN", "€79.90","Decorazioni casa");
-        document.body.appendChild( pinJungle2.obj() );
-
-        pinEtnico1 = new Pin(4500,1000, "NAMMARO", "€249.90","Tavoli");
-        document.body.appendChild( pinEtnico1.obj() );
-
-        pinEtnico2 = new Pin(4000,1130, "BARLAST", "€80.90","Decorazioni casa");
-        document.body.appendChild( pinEtnico2.obj() );
-
-        pinIndustriale1 = new Pin(5500,850, "LANDSKRONA", "€2490.90","Divani");
-        document.body.appendChild( pinIndustriale1.obj() );
-
-        pinIndustriale2 = new Pin(5700,1200, "TARSELE", "€100.90","Tavoli");
-        document.body.appendChild( pinIndustriale2.obj() );
-
-        pinMediterraneo = new Pin(6600,1520, "AZUL", "€350.90","Sedie e poltrone");
-        document.body.appendChild( pinMediterraneo.obj() );
-
-        pinVintage1 = new Pin(7800,1500, "BILD", "€50.90","Decorazioni casa");
-        document.body.appendChild( pinVintage1.obj() );
-
-        pinVintage2 = new Pin(7940,900, "PINNTORP", "€189.90","Tavoli");
-        document.body.appendChild( pinVintage2.obj() );
-
-        pinMinimalista = new Pin(9300,1000, "MINIMAL", "€249.90","Sedie e poltrone");
-        document.body.appendChild( pinMinimalista.obj() );
-
-        pinShabby = new Pin(10500,1100, "KASBAH", "€69.90","Sedie e poltrone");
-        document.body.appendChild( pinShabby.obj() );
-
-        pinScandinavo = new Pin(11750,850, "THOR", "€120.90","Sedie e poltrone");
-        document.body.appendChild( pinScandinavo.obj() );
+        for(p of pins)
+        {
+            document.body.appendChild( p.obj() );
+        }
         
     };
+
+    setEvents = function () {
+        window.onresize = () => {
+            for(p of pins){
+            p.resize();
+            }
+        }
+    };
+
     init = () => {
         createPins();
+        setEvents();
     };
     init();
 }
